f06f7c62ee3a4150403bf30c5326235a35fb2268
malenso <mfensor@gmail.com>
2016-10-28 13:59:30 -0500

    Added files for brain teaser...
    Because Thomas is needy.

---
+++ WebApplication3/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            int[] myArray = new int[] { 1, 3, 6, 2, 7, 5, 3, 8, 5 };
+
+            // Question #1 - 5
+            //int i = 0;
+            //while (true)
+            //{
+            //    if (myArray[i++] > 6)
+            //        break;
+            //}
+            //this.resultLabel.Text = i.ToString();
+
+            // Question #2 - 58
+            int i = myArray.Length - 1;
+            do
+            {
+                this.resultLabel.Text += myArray[i--].ToString();
+            }
+            while (myArray[myArray[i]] != 2);
+        }
+    }
+}
