1a4fa337b7b633d0545f24ecc3b8dfa00f22ee32
malenso <mfensor@gmail.com>
2016-11-07 19:38:03 -0600

    Postal Calculator Challenge
    Assignment to better understand helper methods.
    
    Acceptance: Width, height and optionally the length of a package.  Accept the shipping method - Ground, Air, Next Day.

---
+++ ChallengePostalCalculatorHelperMethodsV2/Default.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostalCalculatorHelperMethodsV2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void widthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            executeFunctions();
+        }
+
+        protected void heightTextBox_TextChanged(object sender, EventArgs e)
+        {
+            executeFunctions();
+        }
+
+        protected void lengthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            executeFunctions();
+        }
+
+        protected void groundRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            executeFunctions();
+        }
+
+        protected void airRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            executeFunctions();
+        }
+
+        protected void nextDayRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            executeFunctions();
+        }
+
+        private void executeFunctions()
+        {
+            double packageVolume = calculatePackageVolume();
+            double shippingMultiplier = getShippingMethodMultiplier();
+            double price = packageVolume * shippingMultiplier;
+
+            printResult(price);
+        }
+
+        private double calculatePackageVolume()
+        {
+            double width = 1;
+            double height = 1;
+            double length = 1;
+
+            double.TryParse(this.widthTextBox.Text, out width);
+            double.TryParse(this.heightTextBox.Text, out height);
+            double.TryParse(this.lengthTextBox.Text, out length);
+
+            double volume = width * height * length;
+
+            return volume;
+        }
+
+        private double getShippingMethodMultiplier()
+        {
+            if (this.groundRadioButton.Checked) return .15;
+            else if (this.groundRadioButton.Checked) return .25;
+            else return .45;
+        }
+
+        private void printResult(double price)
+        {
+            this.resultLabel.Text = String.Format("It will cost {0:C} to ship your package.", price);
+        }
+    }
+}
---
+++ ChallengePostalCalculatorHelperMethodsV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChallengePostalCalculatorHelperMethodsV2.Global" Language="C#" %>
