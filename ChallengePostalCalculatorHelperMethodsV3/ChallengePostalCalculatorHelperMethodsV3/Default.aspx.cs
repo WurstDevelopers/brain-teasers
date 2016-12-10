33102836befdd3012028123f98f53ffba7a175be
Sobieck00 <thomas@sobieck.com>
2016-12-10 06:43:04 -0600

    Little refactorings.

--- ChallengePostalCalculatorHelperMethodsV3/ChallengePostalCalculatorHelperMethodsV3/Default.aspx.cs
+++ ChallengePostalCalculatorHelperMethodsV3/ChallengePostalCalculatorHelperMethodsV3/Default.aspx.cs
@@ -40,50 +40,65 @@ namespace ChallengePostalCalculatorHelperMethodsV3
             this.arrangeFunctionCalls();
         }
 
+        private class Parcel
+        {
+            public double Width { get; set; }
+            public double Heigth { get; set; }
+            public double Length { get; set; }
+
+            public double Volume
+            {
+                get
+                {
+                    return Width * Heigth * Length;
+                }
+            }
+        }
+
         private void arrangeFunctionCalls()
         {
             double width;
             double height;
             double length;
 
-            //check if values exist
+            //check if values exist 
+            // Thomas - I don't like this code. I can't tell you why... I need to think about it... Maybe because it repeats so much.
             if (!double.TryParse(this.widthTextBox.Text, out width)) return;
             if (!double.TryParse(this.heightTextBox.Text, out height)) return;
-            if (!double.TryParse(this.lengthTextBox.Text, out length)) length = 1;
+            if (!double.TryParse(this.lengthTextBox.Text, out length)) length = 1; //why default to 1?
 
-            if (this.checkSelectedShippingMethod())
+            if (isShippingMethodSelected())
             {
-                double productVolume = this.calculateProductVolume(width, height, length);
-                double costToShip = this.calculateCostToShip(productVolume);
+                var parcel = new Parcel
+                {
+                    Width = width,
+                    Heigth = height,
+                    Length = length
+                };
+
+                double costToShip = this.calculateCostToShip(parcel);
                 this.printCostToShip(costToShip);
             };
         }
 
-        private double calculateProductVolume(double width, double height, double length)
+        private bool isShippingMethodSelected()
         {
-            double volume = width * height * length;
-            return volume;
+            if (groundRadioButton.Checked || airRadioButton.Checked || nextDayRadioButton.Checked)
+            {
+                return true;
+            }
+
+            return false;
         }
 
-        private bool checkSelectedShippingMethod()
+        private double calculateCostToShip(Parcel parcel)
         {
-            bool shippingMethodSelected;
-
-            if (this.groundRadioButton.Checked || this.airRadioButton.Checked || this.nextDayRadioButton.Checked) shippingMethodSelected = true;
-            else shippingMethodSelected = false;
-
-            return shippingMethodSelected;
-        }
-
-        private double calculateCostToShip(double productVolume)
-        {
-            double multiplier;
+            double multiplier = .45;
 
             if (this.groundRadioButton.Checked) multiplier = .15;
             else if (this.airRadioButton.Checked) multiplier = .25;
-            else multiplier = .45;
 
-            return productVolume * multiplier;
+            return parcel.Volume * multiplier;
         }
 
         private void printCostToShip(double costToShip)
